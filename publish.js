278b26628d17a79d6a60c55024a4bdf07978df43
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2022-02-04 05:33:31 +0800

    add path correction for ghpages

--- publish.js
+++ publish.js
@@ -1,4 +1,16 @@
 import ghpages from "gh-pages";
+import fs from "fs";
+
+const indexHtml = fs.readFileSync("./dist/index.html", "utf8");
+
+const regex = /="\//g;
+if (regex.test(indexHtml)) {
+  console.log("Found relative path in index.html, fixing...");
+  const newHtml = indexHtml.replace(/="\//g, '="./');
+  console.log("Writing new index.html...");
+  fs.writeFileSync("./dist/index.html", newHtml);
+  console.log("Done!");
+}
 
 console.log("Start publishing...");
 ghpages.publish("dist", function (err) {
